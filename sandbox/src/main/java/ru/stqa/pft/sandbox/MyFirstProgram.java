30fe5595fe2bfb2235677b65f8a28e1e7f44ba6d
onvita <onvita123456@gmail.com>
2019-07-03 01:39:47 +0700

    Промежуточный результат

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println("Hello,"+" world!");
+ int l=8;
+ int s=l*l;
+    System.out.println("Площадь квадрата со стороной "+ l + "=" + s);
   }
 }

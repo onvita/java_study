d4018c7f4176f08cf712d381c75cf23a9daf580a
onvita <onvita123456@gmail.com>
2019-07-04 23:38:18 +0700

    Авторизация перенесена в инициализацию фикстуры

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -13,10 +13,6 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -25,6 +21,11 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
@@ -38,7 +39,6 @@ public class GroupCreationTests {
     wd.findElement(By.name("group_footer")).sendKeys("test13");
     wd.findElement(By.name("submit")).click();
     wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.linkText("Logout")).click();
   }
 
   @AfterMethod(alwaysRun = true)

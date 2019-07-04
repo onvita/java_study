80a5e8566afdf867ea2b313028f8d19e8bf3c912
onvita <onvita123456@gmail.com>
2019-07-05 01:52:17 +0700

    Создан и отрефакторентТест для создания группы. Создан объект GroupData

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,4 +1,3 @@
-
 package ru.stqa.pft.addressbook;
 
 import java.util.concurrent.TimeUnit;
@@ -14,31 +13,55 @@ public class GroupCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    login("admin", "secret");
   }
 
-  @Test
+   @Test
   public void testGroupCreation() throws Exception {
 
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test11", "test12", "test13"));
+    submitGroupCreation();
+    gotoGroupPage();
+  }
+// Ыспомогательные методы
+
+  // Метода авторизации
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+  // Метода для перехода на стр. групп
+  private void gotoGroupPage() {
     wd.findElement(By.linkText("groups")).click();
+  }
+  // Метод для перехода на стр. создания новой группы ( Нажатие кнопки new)
+  private void initGroupCreation() {
     wd.findElement(By.name("new")).click();
+  }
+
+  // Заполнить поля  на форме создания группы
+  private void fillGroupForm(GroupData groupDat) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("test11");
+    wd.findElement(By.name("group_name")).sendKeys(groupDat.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("test12");
+    wd.findElement(By.name("group_header")).sendKeys(groupDat.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("test13");
+    wd.findElement(By.name("group_footer")).sendKeys(groupDat.getFooter());
+  }
+
+  // Метод - подтвержаем создание новой группы- (нажатие кнопки подтверждения)
+  private void submitGroupCreation() {
     wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)

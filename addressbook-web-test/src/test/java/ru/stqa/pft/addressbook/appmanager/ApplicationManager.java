b06a045bf2a8eea4e2d6e0999ec39bd781d017fb
onvita <onvita123456@gmail.com>
2019-07-06 23:52:22 +0700

    Создан SessionHelper, в него перенесен метод Логина
    Окончание занятия 2.7

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -10,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private WebDriver wd;
+  private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
@@ -19,7 +20,8 @@ public class ApplicationManager {
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    login("admin", "secret");
+    sessionHelper= new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
   public void stop() {
@@ -44,16 +46,6 @@ public class ApplicationManager {
     }
   }
 
-  // Метод авторизации
-  public void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
 
   public GroupHelper getGroupHelper() {
     return groupHelper;

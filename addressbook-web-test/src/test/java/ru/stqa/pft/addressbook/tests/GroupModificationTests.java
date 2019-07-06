007ee0bf2f22d806a1e22f7bdf65878fa73abc3f
onvita <onvita123456@gmail.com>
2019-07-07 02:19:28 +0700

    Создан тест GroupModificationTests
    Оокнчание занятия 2.9

---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+  @Test
+  public void testGroupModification ()
+  { app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test11is", "test12is", "test13is"));
+    app.getGroupHelper().submitGroupModification();
+    app.getNavigationHelper().gotoGroupPage();
+  }
+}

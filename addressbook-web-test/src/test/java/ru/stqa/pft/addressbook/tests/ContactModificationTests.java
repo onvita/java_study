7d2fbbb2b9a6251005de269fa2784417ddba4709
onvita <onvita123456@gmail.com>
2019-07-10 21:19:13 +0700

    Создан тест ContactModificationTests
    Тест проверяет редактирование контакта переходом со страницы просмотар контакта
    Добавлены соот. методы в  ContactHelper

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,13 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactModificationTests extends TestBase {
 
   @Test
+  // Тест проверяет редактирование контакта через форму модификации, переход к которой осуществляется
+  // со страницы просмотра контакта,нажатием кнопки Modify
   public void testContactModification ()
   {
-
-
+   app.getContactHelper().gotoContaktViewPage();
+   app.getContactHelper().submitContactModifiy();
+   app.getContactHelper().fillContactForm(new ContactData("Степан", "", "Пирожков", "Ул.Попова", "22-33-44", "+7 444-444-44-44", "" , "ссс@mail.ru", "", ""));
+   app.getContactHelper().submitContactUpdate();
+   app.getContactHelper().returnToHomePage();
   }
 }

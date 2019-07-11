1e50648b76ab1a5af7e2ffb0749c26ddbd4282ed
onvita <onvita123456@gmail.com>
2019-07-12 01:46:39 +0700

    Добавлен класс Equality  в песочнице
    Занятие 3.4

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Equality {
+
+  public static void main (String[] arrgs) {
+    String s1 = "firefox";
+    String s2 = "firefox";
+
+    System.out.println(s1 ==s2);
+    System.out.println(s1.equals(s2));
+  }
+}

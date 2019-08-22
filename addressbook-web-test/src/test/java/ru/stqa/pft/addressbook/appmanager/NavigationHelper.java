54403e4b84b89bdea6f1359272e1712dde8c890c
onvita <onvita123456@gmail.com>
2019-08-22 16:23:36 +0700

    Оптимизированные переходы между страницами
    В NavigationHelper в методах gotoGroupPage и gotoHomePage ( создала новый, т.к. раньше его у меня не было) проверяется, где находимся и только после этого переход если нужно.
    Лекция 3.9

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -14,8 +14,23 @@ public class NavigationHelper extends HelperBase{
 
   // Метода для перехода на стр. групп
   public void gotoGroupPage() {
+    //  Выходим из метода  gotoGroupPage если выполнятся условия
+    if (isElementPresent(By.tagName("h1"))   // На странице найден эленент h1
+        && wd.findElement(By.tagName("h1")).getText().equals ("Groups") // и  значение текста его равно  "Groups"
+        && isElementPresent(By.name("new"))) { //  И есть элемент с названием "new"
+      return; // выходим
+    }
+
     click(By.linkText("groups"));
     }
 
+  // Метода для перехода на  главную страницу
+  public void gotoHomePage() {
+    //  Выходим из метода  gotoHomePage если выполнятся условия
+   if (isElementPresent(By.id("maintable")))   // На странице есть таблица id="maintable"
+    { return; // выходим
+    }
 
+    click(By.linkText("home"));
+  }
 }
